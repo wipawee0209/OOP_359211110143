f10cfc0d92d04f524adce2ef8d718db63376cfc9
wipawee0209 <rosewhite180209@gmail.com>
2017-11-24 17:12:29 +0700

    Successfuly information data

---
+++ src/ooplab/MyfirstJAVA.java
@@ -0,0 +1,10 @@
+package ooplab;
+
+public class MyfirstJAVA {
+    public static void main(String[] args) {
+        System.out.println("Hello Wordl,I'm wipawee");
+        System.out.println("Information systsm");
+
+
+    }
+}

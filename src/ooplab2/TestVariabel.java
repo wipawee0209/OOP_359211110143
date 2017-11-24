7bf82d4bf58fe66ffb66d6a3b8c74f01e78eee45
wipawee0209 <rosewhite180209@gmail.com>
2017-11-24 16:41:24 +0700

    Initial commit

---
+++ src/ooplab2/TestVariabel.java
@@ -0,0 +1,43 @@
+package ooplab2;
+
+public class TestVariabel {
+    public static void main(String[] args) {
+        // integer
+        int i;
+        i =10;
+        System.out.print("i="+i);
+        int j = 20;
+        System.out.print("j ="+j);
+        System.out.print("i+j+"+(i+j));
+
+        //Double
+        double v = 15.15;
+        System.out.print(v);
+        System.out.print(v*10);
+
+        //Character
+        char c = 'T';
+        System.out.print(c);
+
+        //String
+        String s ="Hello, Word";
+        System.out.print(s);
+        String name = " I'm wipawee";
+        String msg = s+ " "+name;
+        System.out.print(msg);
+
+        String msg2 = s.concat(" "+name);
+        System.out.println(msg2);
+        System.out.println(msg.length());
+        System.out.println(msg.toUpperCase());
+        System.out.println(msg.toLowerCase());
+
+        //  boolean
+        boolean b = true;
+        boolean b1 = false;
+        System.out.println(b);
+        System.out.println(b1);
+        System.out.println(b1);
+
+    }//main
+}//class
